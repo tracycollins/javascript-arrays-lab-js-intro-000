1ac72e1a4481c42a094541bb33b7e28630300869
Tracy Collins <tc@threeceemedia.com>
2020-02-27 05:14:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function prependKitten(kitten) {
 }
 
 function removeLastKitten(kitten) {
-  return kittens.slice(0,kittens.lenth-1)
+  return kittens.slice(kittens.lenth-1)
 }
 
 function destructivelyPrependKitten(kitten) {
