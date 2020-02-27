b1f4f204b070eb35bafb041727e73d793a491481
Tracy Collins <tc@threeceemedia.com>
2020-02-27 05:08:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(kitten) {
   return kittens.push(kitten)
 }
+
+function destructivelyPrependKitten(kitten) {
+  return kittens.unshift(kitten)
+}
